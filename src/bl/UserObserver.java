3209a6987d627fd0d17b1a3272835d8fdfc0731e
JonatanJireh <92609410+JonatanJireh@users.noreply.github.com>
2024-05-28 15:58:56 -0400

    Update UserObserver.java

--- src/bl/UserObserver.java
+++ src/bl/UserObserver.java
@@ -1,5 +1,6 @@
 /*
 Patrón de Comportamiento (Observer): UserObserver implementa el patrón Observer para notificar cambios en los datos a los observadores.
+I: Interface Segregation Principle (ISP) –Se utilizan interfaces (UserObserver) para asegurar que las clases no sean forzadas a implementar métodos que no necesitan.
 */
 package bl;
 
